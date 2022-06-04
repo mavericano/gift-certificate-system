17b960d64f6ccc7e4a38eaf98ac94c62b20ed60a
Ivan Gavrilovich <ivan_gavrilovich@epam.com>
2022-06-04 15:39:51 +0300

    Required changes

--- core/src/main/java/com/epam/esm/core/converter/GiftCertificateMapper.java
+++ core/src/main/java/com/epam/esm/core/converter/GiftCertificateMapper.java
@@ -1,15 +1,25 @@
-package com.epam.esm.core.converter.impl;
+package com.epam.esm.core.converter;
 
 import com.epam.esm.core.dto.GiftCertificateDto;
 import com.epam.esm.core.entity.GiftCertificate;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = {TagMapper.class})
 public interface GiftCertificateMapper {
 
     GiftCertificateMapper INSTANCE = Mappers.getMapper(GiftCertificateMapper.class);
 
+    //@Mapping(target = "tagSet", ignore = true)
     GiftCertificateDto certificateToCertificateDto(GiftCertificate giftCertificate);
+
+    //@Mapping(target = "tagSet", ignore = true)
     GiftCertificate certificateDtoToCertificate(GiftCertificateDto giftCertificateDto);
+
+//    @AfterMapping
+//    default void mapTags(GiftCertificate giftCertificate, GiftCertificateDto giftCertificateDto) {
+//        giftCertificate.getTagSet().forEach(tag -> giftCertificateDto.getTagSet().add(TagMapper.INSTANCE.tagToTagDto(tag)));
+//    }
 }
--- core/src/main/java/com/epam/esm/core/dto/TagDto.java
+++ core/src/main/java/com/epam/esm/core/dto/TagDto.java
@@ -1,4 +1,25 @@
 package com.epam.esm.core.dto;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TagDto {
+    private long id;
+
+    @NotBlank
+    private String name;
+
+    @JsonIgnore
+    private List<GiftCertificateDto> certificates;
 }
